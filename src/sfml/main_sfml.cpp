8c224dec490c487e050eb9d51b5f45e9c050c3ef
David Churchill <dave.churchill@gmail.com>
2021-03-15 06:47:54 -0230

    ui update

--- src/sfml/main_sfml.cpp
+++ src/sfml/main_sfml.cpp
@@ -79,12 +79,12 @@ GameState GetZergStartState()
     state.addUnit(ActionType("Drone"));
     state.addUnit(ActionType("Overlord"));
     state.addUnit(ActionType("Hatchery"));
-    state.setMinerals(50);
+    //state.setMinerals(50);
 
     GameState state2(state);
 
     BuildOrder bo;
-    std::vector<std::string> bos = { "Drone", "Drone", "Drone", "Drone", "Overlord", "Drone", "Drone", "Drone", "Drone", "Hatchery", "SpawningPool", "Drone", "Drone", "Hatchery", "Extractor", "Drone", "Drone", "Drone", "Overlord" };
+    std::vector<std::string> bos = {  };
     for (size_t a = 0; a < bos.size(); a++)
     {
         bo.add(ActionType(bos[a]));
@@ -112,7 +112,7 @@ void test()
 
     bos = { "SiegeTank", "SiegeTank" };
     bos = { "Drone", "Drone", "Drone", "Drone", "Drone", "Overlord", "Drone", "Drone", "Drone", "Hatchery", "SpawningPool", "Drone", "Extractor", "Drone", "Drone", "Drone", "Drone", "Drone", "Drone", "HydraliskDen", "Drone", "Overlord", "Drone", "Drone", "Drone", "Hydralisk", "Hydralisk", "Hydralisk","Hydralisk", "Hydralisk","Hydralisk","Hydralisk","Hydralisk", "Hydralisk","Hydralisk","Hydralisk","Hydralisk", "Hatchery", "Extractor" };
-    bos = { "Lair", "Extractor", "Drone", "Drone", "Zergling", "Zergling", "Zergling", "Drone", "Drone", "Drone", "Drone", "Overlord", "Drone", "Overlord", "Drone", "Overlord", "Spire", "Overlord", "Drone", "Drone", "Drone", "Drone", "Drone", "Mutalisk", "Mutalisk", "Mutalisk", "Mutalisk", "Mutalisk", "Mutalisk", "Mutalisk", "Mutalisk", "Mutalisk", "Mutalisk", "Mutalisk", "Mutalisk" };
+    bos = { "Drone", "Drone", "Drone", "Drone", "Overlord", "Drone", "Drone", "Drone", "Drone", "Hatchery", "SpawningPool", "Drone", "Drone", "Hatchery", "Extractor", "Drone", "Drone", "Drone", "Overlord", "Lair", "Extractor", "Drone", "Drone", "Zergling", "Zergling", "Zergling", "Drone", "Drone", "Drone", "Drone", "Overlord", "Drone", "Overlord", "Drone", "Overlord", "Spire", "Overlord", "Drone", "Drone", "Drone", "Drone", "Drone", "Mutalisk", "Mutalisk", "Mutalisk", "Mutalisk", "Mutalisk", "Mutalisk", "Mutalisk", "Mutalisk", "Mutalisk", "Mutalisk", "Mutalisk", "Mutalisk" };
 
     std::vector<ActionType> buildOrder;
 
